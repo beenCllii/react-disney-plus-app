96d52d1f7ef3738d9bce5404ef5f877ba90afc81
beenzzang <beenzzang00@gmail.com>
2024-07-20 16:13:09 +0900

    video rows add

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,13 @@
 import axios from '../api/axios';
 import React, { useCallback, useEffect, useState } from 'react'
+import './Row.css'
+import MovieModal from './MovieModal';
 
 const Row = ({title,id,fetchUrl}) => {
 
   const [movies,setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
 
   const fetchMovieDate = useCallback( async () => {
     const responses = await axios.get(fetchUrl);
@@ -14,13 +18,19 @@ const Row = ({title,id,fetchUrl}) => {
     fetchMovieDate();
   },[fetchMovieDate])
 
+  const handleClick = (movie) => {
+     setModalOpen(true);
+     setMovieSelected(movie);
+  }
+
   
   return (
     <div>
        <h2>{title}</h2>
       <div className='slider'>
         <div className='slider_arrow-left'>
-          <span className='arrow'>
+          <span className='arrow' 
+          onClick={()=>{document.getElementById(id).scrollLeft -= window.innerWidth - 80}}>
             {'<'}
           </span>
         </div>
@@ -29,15 +39,22 @@ const Row = ({title,id,fetchUrl}) => {
             <img key='{movie.id}' 
             className='row_poster' 
             src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} 
-            alt={movie.id}/>
+            alt={movie.id}
+            onClick={()=> handleClick(movie)}/>
           ))}
         </div>
         <div className='slider_arrow-right'>
-          <span className='arrow'>
+          <span className='arrow' 
+          onClick={()=>{document.getElementById(id).scrollLeft += window.innerWidth - 80}}>
             {'>'}
           </span>
         </div>
       </div>
+
+      {setModalOpen && 
+      <MovieModal {...movieSelected} modalOpen={modalOpen}
+      />
+      }
     </div>
   )
 }
